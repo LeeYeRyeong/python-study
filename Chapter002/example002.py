1ee3f68fe64052e0fdf8b4c9f34103bf68f744c2
ryxxng <i876838@gmail.com>
2025-02-26 19:58:28 +0900

    feat : input() function error

---
+++ Chapter002/example002.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+result = a + b
+print(a,'+',b,'=',result)
+# a,b를 모두 문자열 취급해서 오류발생
+result = a - b
+print(a,'-',b,'=',result)
+result = a * b
+print(a,'*',b,'=',result)
+result = a / b
+print(a,'/',b,'=',result)
